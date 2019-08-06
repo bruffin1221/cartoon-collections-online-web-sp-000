e5f28a439839ad6eb8bbb75b4193a32fcb2f5604
Bruce Ruffin <bruffin1221@gmail.com>
2019-08-06 23:42:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,8 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.detect do |name|
+    name=cheese_types
 end
